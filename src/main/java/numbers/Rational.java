9504aa837b3a4f47935c6820cfcd40deac79c523
Nicole Hernandez <nicole.2021.nh@gmail.com>
2024-04-10 11:29:04 -0500

    changes made in Rational

--- src/main/java/numbers/Rational.java
+++ src/main/java/numbers/Rational.java
@@ -1,17 +1,58 @@
 package numbers;
 
-public class Rational 
-{
+public class Rational extends Number implements Comparable<Number> 
+{    
     private int theNumerator;
-    private int theDenominator;      
+    private int theDenominator;
 
-    public Rational() {
-        this(0);
+    public Rational() { 
+        this(0, 1); 
+    }
+    
+
+    public Rational(int a) { 
+        this(a, 1); 
+    }
+    
+
+    public static int VerifiedMultiplication(int a, int b){
+        int product = a * b;
+        double x = a; double y = b;
+        if(product != x * y) throw new IllegalArgumentException("Overflow when multiplying");
+
+        return product;
     }
 
-    public Rational(int a) {
-        this(a, 1);
+
+    public static int VerifiedAddition(int a, int b){
+        int sum = a + b;
+        double x = a; double y = b;
+        if(sum != x + y) throw new IllegalArgumentException("Overflow when adding");
+        return sum;
     }
+
+
+    private static int gcd(int n, int d) {
+        double a = n; a = Math.abs(a);
+        double b = d; b = Math.abs(b);
+        return (int)(gcd(b, a % b)); 
+    }
+
+    private static double gcd(double n, double d) {
+        if (d == 0) {
+        return n;
+        }
+        else {
+        return gcd(d, n % d); 
+        }
+    }
+
+
+    private static int lcd(int n, int d) {
+        int GCD = gcd(n, d); 
+        return (n / GCD) * d; 
+    }
+    
 
     public Rational(int a, int b) {  
         if (b == 0) {
@@ -24,8 +65,9 @@ public class Rational
     }
 
     public Rational(Rational r) {
-        this.theNumerator = r.theNumerator;
-        this.theDenominator = r.theDenominator;
+        if (r == null) throw new NullPointerException("A rational object can't be null");
+        theNumerator = r.theNumerator;
+        theDenominator = r.theDenominator;
     }
 
     public int numerator() {
@@ -46,26 +88,20 @@ public class Rational
         }
         return new Rational(this.theDenominator, this.theNumerator);
     }
-
-    public Rational times(Rational r) {
-        int newNumerator = this.theNumerator * r.theNumerator;
-        int newDenominator = this.theDenominator * r.theDenominator;
-        return new Rational(newNumerator, newDenominator);
+    
+    
+    public Rational times(Rational r){
+        if (r == null) return null;
+        return new Rational(VerifiedMultiplication(this.theNumerator, r.theNumerator), VerifiedMultiplication(this.theDenominator, r.theDenominator));
     }
 
-    public Rational plus(Rational r) {
-        // Cross-multiply to find a common denominator
-        int commonDenominator = this.theDenominator * r.theDenominator;
-
-        // Calculate new numerators
-        int newNumerator1 = this.theNumerator * r.theDenominator;
-        int newNumerator2 = r.theNumerator * this.theDenominator;
-
-        // Sum of numerators
-        int sumNumerator = newNumerator1 + newNumerator2;
-
-        // Create a new Rational with the sum
-        return new Rational(sumNumerator, commonDenominator);
+    public Rational plus(Rational r){
+        if (r == null) return null;
+        int b = lcd(theDenominator, r.theDenominator);
+        int x = VerifiedMultiplication(theNumerator, (b / theDenominator));
+        int y = VerifiedMultiplication(r.theNumerator, (b / r.theDenominator));
+        int a = VerifiedAddition(x, y);
+        return new Rational(a, b);
     }
 
     public Rational minus(Rational r) {
@@ -82,7 +118,7 @@ public class Rational
         // Create a new Rational with the difference
         return new Rational(differenceNumerator, commonDenominator);
     }
-
+    
     public Rational dividedBy(Rational r) {
         // Ensure the denominator of the divisor is not zero
         if (r.theNumerator == 0) {
@@ -95,28 +131,39 @@ public class Rational
     }
 
     public Rational raisedToThePowerOf(int n) {
-        // Check for the case where the numerator is zero and the power is negative
         if (this.theNumerator == 0 && n < 0) {
             throw new IllegalArgumentException("Cannot raise zero to a negative power.");
         } else if (n < 0) {
-            // For a negative power, swap numerator and denominator and raise to the positive power
             return new Rational((int) Math.pow(this.theDenominator, -n), (int) Math.pow(this.theNumerator, -n));
         } else if (n > 0) {
-            // Raise to the positive power
             return new Rational((int) Math.pow(this.theNumerator, n), (int) Math.pow(this.theDenominator, n));
         } else {
-            // Power of zero, result is 1
             return new Rational(1);
         }
     }
-
+   
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Rational rational = (Rational) o;
         return this.theNumerator == rational.theNumerator && this.theDenominator == rational.theDenominator;
     }
-    
+    @Override
+    public double doubleValue() {
+        return (double) theNumerator / theDenominator;
+    }
+    @Override
+    public int intValue() {
+        return theNumerator / theDenominator;
+    }
+    @Override
+    public long longValue() {
+        return (long) theNumerator / theDenominator;
+    }
+    @Override
+    public float floatValue() {
+        return (float) theNumerator / theDenominator;
+    }
     public boolean greaterThan(Number n) {
         if (n instanceof Integer) {
             return this.theNumerator > n.intValue() * this.theDenominator;
@@ -160,10 +207,8 @@ public class Rational
 
     public String toString() {
         if (this.theDenominator == 1) {
-            // If the denominator is 1, print only the numerator
             return Integer.toString(this.theNumerator);
         } else {
-            // Otherwise, print the numerator/denominator
             return this.theNumerator + "/" + this.theDenominator;
         }
     }
@@ -175,5 +220,12 @@ public class Rational
             a = temp;
         }
         return Math.abs(a);
+    }
+    
+    @Override
+    public int compareTo(Number n) {
+        if(equals(n)) { return 0; }
+        else if(greaterThan(n)) { return 1; }
+        else { return -1; }
     }
 }
